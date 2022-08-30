b7cab621d42bb40bdd4afa8b157bdddf5f2309ec
gurjung <gurjung997@gmail.com>
2022-08-31 02:11:26 +0530

    feat: enable smooth scrolling

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -37,7 +37,7 @@ const Main = () => {
                 <FaGithub />
               </div>
             </a>
-            <Link href="/contact">
+            <Link href="/#contact">
               <div className="p-6 duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-110">
                 <AiOutlineMail />
               </div>
