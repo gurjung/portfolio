4bcf4f273647e7d57fe26db4368a8e1461007454
gurjung <gurjung997@gmail.com>
2026-09-22 21:01:23 +0530

    fix(layout): add suppressHydrationWarning to body tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={raleway.variable}>
-      <body className="antialiased">
+      <body className="antialiased" suppressHydrationWarning>
         {children}
         <ToastProvider />
       </body>
